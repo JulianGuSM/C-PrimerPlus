950cb3fe6ab48a88506db37b6eb10d58c42e5e5f
JulianGuSM <244901948@qq.com>
2019-02-24 16:57:29 +0800

    upload the project

---
+++ chapter3_7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now actived!\n";
+    cout << "Enter your agent code:____\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou entered" << code << "...\n";
+    cout << "\aCode verified!Proceed with Plan Z3!\n";
+    return 0;
+}
